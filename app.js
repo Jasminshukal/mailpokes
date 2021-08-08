facec19dddd9c0b1cceadbe84d8500630370f85c
Jasmin Shukal <39101720+Jasminshukal@users.noreply.github.com>
2021-08-09 00:33:15 +0530

    heroku port error

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ const server = http.createServer((req, res) => {
   res.end('<h1>Hello World</h1>');
 });
 
-server.listen(port,() => {
-  console.log(`Server running at port `+port);
+var server_port = process.env.YOUR_PORT || process.env.PORT || 80;
+var server_host = process.env.YOUR_HOST || '0.0.0.0';
+server.listen(server_port, server_host, function() {
+    console.log('Listening on port %d', server_port);
 });
